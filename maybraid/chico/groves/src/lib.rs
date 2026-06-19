8f170662bba186fd9e17c2619b4f443d165e3a2e
Liam Makena Monninger <liam@ramate.io>
2026-06-19 05:51:01 -0700

    feat(groves): add Conifer Massives upper-canopy grove
    Resolves #343

--- maybraid/chico/groves/src/lib.rs
+++ maybraid/chico/groves/src/lib.rs
@@ -204,4 +204,8 @@ pub use tropical_undergrowth::{TropicalUndergrowth, TropicalUndergrowthStd};
 #[cfg(feature = "render")]
 pub use unending_jungle::{UnendingJungle, UnendingJungleStd};
 #[cfg(feature = "render")]
-pub use wild_grass::{WildGrass, WildGrassStd};
+pub use wild_grass::{WildGrass, WildGrassStd};pub mod conifer_massives;
+pub use conifer_massives::{
+	ConiferMassivesCell, ConiferMassivesFriendsConifer, ConiferMassivesItem,
+	ConiferMassivesLiamsConifer, ConiferMassivesNorthernConifer, ConiferMassivesTemperateConifer,
+pub use conifer_massives::{ConiferMassives, ConiferMassivesStd};
