a1551aa54874d32deb89e6a5514134435875382e
Liam Makena Monninger <liam@ramate.io>
2026-08-03 11:58:59 +0100

    feat: sketch les halles.

--- maybraid/richmond/buildings/src/storeys/les_halles.rs
+++ maybraid/richmond/buildings/src/storeys/les_halles.rs
@@ -1,6 +1,14 @@
 //! Les Halles storey.
 //!
+//! The basic Les Halles layout is an outer ring gallery with an inner ring balcony.
+//! The outer ring gallery is meant to reflect commercial space,
+//! while the inner ring balcony is the walking access to the commercial space.
 //!
+//! The Les Halles floor plan...
+//! - Uses the full rectangular width and height of the confines.
+//! - Decides reasonable widths for the gallery and balcony.
+//! - Adds reasonable additional doors and windows in the open set along the gallery.
+//! - Chooses whether to allocate shafts (for stairs) in the four corners or in the middle of each side.
 
 pub mod floor_plan;
 pub mod full_storey;
