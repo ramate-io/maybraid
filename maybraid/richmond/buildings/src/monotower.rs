56ffb2c35f403cdb2dae77aa4a90000da6b0052c
Liam Makena Monninger <liam@ramate.io>
2026-08-07 09:32:18 +0100

    chore: sketch monotower.

---
+++ maybraid/richmond/buildings/src/monotower.rs
@@ -0,0 +1,6 @@
+//! A monotower is a tower of a single type of storey floor plan.
+//!
+//! Because the mappings of shafts onto the floor plans to the storeys are one-to-one,
+//! we can ensure good continuity.
+//!
+//! The tower has the responsibility of filling in proper shafts.
