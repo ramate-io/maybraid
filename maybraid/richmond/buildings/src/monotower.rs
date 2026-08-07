cec1772d0705fd5cb6c7307c2e68df69eddecf4e
Liam Makena Monninger <liam@ramate.io>
2026-08-07 09:38:13 +0100

    chore: sketch monotower.

--- maybraid/richmond/buildings/src/monotower.rs
+++ maybraid/richmond/buildings/src/monotower.rs
@@ -3,4 +3,15 @@
 //! Because the mappings of shafts onto the floor plans to the storeys are one-to-one,
 //! we can ensure good continuity.
 //!
-//! The tower has the responsibility of filling in proper shafts.
+//! The monotower has the responsibility of filling in proper shafts.
+//!
+//! When monotowers fit and AaBb3d, they choose a floor height from a range
+//! and then adjust to have a whole number of storeys.
+//!
+//! Monotowers typically follow a Parameterized -> FloorPlan -> Full approach as well
+//! wherein floor plans include storey floor plans and stairs in the shafts. Then usage
+//! areas are filled using the same pattern as a particular full variant per storey.
+//!
+//! Note that, going forward, we should probably make clearer the *UsagePlan concept in stories as well,
+//! breaking out the UsagePlan paint-on logic from the full-storey struct itself in a nice
+//! reusable way.
