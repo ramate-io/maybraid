00bd2c89e683ab07a0e96eb41a174fb63474d098
Liam Makena Monninger <liam@ramate.io>
2026-08-25 13:26:14 +0100

    chore: high bushes need larger bands.

--- maybraid/chico/groves/src/wandering_acacia.rs
+++ maybraid/chico/groves/src/wandering_acacia.rs
@@ -286,8 +286,8 @@ mod vc {
 	};
 
 	pub const WANDERING_ACACIA_STRUCTURAL_HIGH_FACTOR: f32 = 5.0;
-	pub const WANDERING_ACACIA_STRUCTURAL_MEDIUM_FACTOR: f32 = 10.0;
-	pub const WANDERING_ACACIA_STRUCTURAL_LOW_FACTOR: f32 = 20.0;
+	pub const WANDERING_ACACIA_STRUCTURAL_MEDIUM_FACTOR: f32 = 20.0;
+	pub const WANDERING_ACACIA_STRUCTURAL_LOW_FACTOR: f32 = 30.0;
 
 	#[derive(Clone, Debug, Args)]
 	#[command(rename_all = "kebab-case")]
